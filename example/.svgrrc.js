669475dd146b289aa963b0c8a456ed33ddceaefd
Frank Cooke <frankcooke79@gmail.com>
2024-12-19 23:36:33 -0500

    feat: migrate example app to latest version of next.js

--- example/.svgrrc.js
+++
@@ -1,8 +0,0 @@
-// Optionally you can also pass your svgr config in next.config.ts directly to 'withSvgr'
-module.exports = {
-  titleProp: true,
-  icon: true,
-  svgProps: {
-    height: '100%',
-  }
-};
